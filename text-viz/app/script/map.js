1354ceb332b3bfec6f0dd682701bfaae8fe1490f
Bianca Aguglia <bianca.aguglia@gmail.com>
2022-08-23 17:07:14 -0400

    add timeline-slider

--- text-viz/app/script/map.js
+++ text-viz/app/script/map.js
@@ -1,8 +1,10 @@
 let locations = {'buffalo': {'lat': 42.88,
-                            'long': -78.87
-}}
+                            'long': -78.87},
+                  'rome': {'lat': 41.9028,
+                          'long': 12.4964}}
 
-let map = L.map('map').setView([locations['buffalo']['lat'], locations['buffalo']['long']], 6);
+
+let map = L.map('map').setView([locations['rome']['lat'], locations['rome']['long']], 2);
 
 // L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 //     maxZoom: 19,
@@ -10,9 +12,22 @@ let map = L.map('map').setView([locations['buffalo']['lat'], locations['buffalo'
 // }).addTo(map);
 
 const mainLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
-  minZoom: 3,
+  minZoom: 1,
   maxZoom: 17,
   attribution: '&copy; <a href="https://carto.com/">carto.com</a> contributors'
 });
 
 mainLayer.addTo(map);
+
+
+
+/* slider */
+var timelineSlider = document.getElementById('timeline-slider')
+// var timelineSliderAfter = window.getComputedStyle(element, ':after')
+var output = document.getElementById('year')
+output.innerHTML = timelineSlider.value
+
+// update the current slider value (each time you drag the slider handle)
+timelineSlider.oninput = function() {
+  output.innerHTML = this.value
+}
